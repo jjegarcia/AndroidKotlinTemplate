7c3644323e8cd245b8276045424009d8a059b2a9
jjegarcia <jjegarcia@hotmail.com>
2023-09-30 14:17:48 +0100

    use state flow to pass arguments to ui

--- app/src/main/java/com/example/androidkotlintemplate/MainViewModel.kt
+++ app/src/main/java/com/example/androidkotlintemplate/MainViewModel.kt
@@ -3,16 +3,21 @@ package com.example.androidkotlintemplate
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 interface MainViewModel {
-    val introImageId: Int
-    val screenName: String
+    val screenData: MutableStateFlow<ScreenData>
 }
 
 @HiltViewModel
-class MainViewModelImpl @Inject constructor( val context: Context) : ViewModel(), MainViewModel {
-    override val introImageId: Int
-        get() = R.drawable.ic_launcher_background
-    override val screenName get() = "Test"
+class MainViewModelImpl @Inject constructor(val context: Context) : ViewModel(), MainViewModel {
+    private val _screenData = MutableStateFlow(
+        ScreenData(
+            "Test",
+            R.drawable.ic_launcher_background,
+        )
+    )
+    override val screenData: MutableStateFlow<ScreenData>
+        get() = _screenData
 }
--- app/src/main/java/com/example/androidkotlintemplate/Screen.kt
+++ app/src/main/java/com/example/androidkotlintemplate/Screen.kt
@@ -3,22 +3,22 @@ package com.example.androidkotlintemplate
 import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 
+data class ScreenData(
+    val screenName: String,
+    val introImageId: Int
+)
 @Composable
 fun Greeting(viewModel: MainViewModelImpl, modifier: Modifier = Modifier) {
+
+    val screenData = viewModel.screenData.collectAsState().value
+
     Text(
-        text = "Hello ${viewModel.screenName}!",
+        text = "Hello ${screenData.screenName}!",
         modifier = modifier
     )
-    Image(painter = painterResource(id = viewModel.introImageId), contentDescription = "Intro Image")
+    Image(painter = painterResource(id = screenData.introImageId), contentDescription = "Intro Image")
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    AndroidKotlinTemplateTheme {
-//        Greeting(MainViewModelImpl(this@MainActivity))
-//    }
-//}
