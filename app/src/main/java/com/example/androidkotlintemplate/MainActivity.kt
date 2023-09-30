bbe3b7002418ac02e591ff8d83f6d7e045517274
jjegarcia <jjegarcia@hotmail.com>
2023-09-30 08:59:06 +0100

    extracted compose Screen

--- app/src/main/java/com/example/androidkotlintemplate/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlintemplate/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidkotlintemplate.ui.theme.AndroidKotlinTemplateTheme
-import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,21 +25,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(viewModel: MainViewModelImpl, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello ${viewModel.screenName}!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidKotlinTemplateTheme {
-        Greeting(MainViewModelImpl())
     }
 }
