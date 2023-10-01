b722d670b6b74faf97949305fbe4fb43c5855441
jjegarcia <jjegarcia@hotmail.com>
2023-10-01 11:14:34 +0100

    using coil's AsyncImage to load image from given URL

--- app/src/main/java/com/example/androidkotlintemplate/MainViewModel.kt
+++ app/src/main/java/com/example/androidkotlintemplate/MainViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.androidkotlintemplate
 
 import android.content.Context
+import androidx.core.net.toUri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import java.net.URI
 import javax.inject.Inject
+
+enum class ApiStatus { LOADING, ERROR, DONE }
 
 interface MainViewModel {
     val screenData: MutableStateFlow<ScreenData>
@@ -12,12 +20,56 @@ interface MainViewModel {
 
 @HiltViewModel
 class MainViewModelImpl @Inject constructor(val context: Context) : ViewModel(), MainViewModel {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<ApiStatus>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<ApiStatus> = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
+    // with new values
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
+
     private val _screenData = MutableStateFlow(
         ScreenData(
             "Test",
-            R.drawable.ic_launcher_background,
+            getImage(null),
+            "https://res.cloudinary.com/demo/image/upload/v1212461204/sample.jpg"
         )
     )
+
+    private fun getImage(uri: URI?) = if (uri == null) R.drawable.ic_launcher_background
+    else
+        getImageFromOnline(uri)
+
+    private fun getImageFromOnline(uri: URI): Int = androidx.core.R.drawable.ic_call_answer
     override val screenData: MutableStateFlow<ScreenData>
         get() = _screenData
+
+    init {
+        getMarsPhotos()
+        if(_photos.value?.isEmpty() == false){
+            _screenData.value.copy(url = "https://res.cloudinary.com/demo/image/upload/v1212461204/sample.jpg"
+            )
+        }
+    }
+
+    private fun getMarsPhotos() {
+
+        viewModelScope.launch {
+            _status.value = ApiStatus.LOADING
+            try {
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = ApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = ApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/androidkotlintemplate/Screen.kt
+++ app/src/main/java/com/example/androidkotlintemplate/Screen.kt
@@ -1,16 +1,20 @@
 package com.example.androidkotlintemplate
 
-import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.core.R.drawable
+import coil.compose.AsyncImage
 
 data class ScreenData(
     val screenName: String,
-    val introImageId: Int
+    val introImageId: Int,
+    val url: String
 )
+
 @Composable
 fun Greeting(viewModel: MainViewModelImpl, modifier: Modifier = Modifier) {
 
@@ -20,5 +24,12 @@ fun Greeting(viewModel: MainViewModelImpl, modifier: Modifier = Modifier) {
         text = "Hello ${screenData.screenName}!",
         modifier = modifier
     )
-    Image(painter = painterResource(id = screenData.introImageId), contentDescription = "Intro Image")
+    AsyncImage(
+        model = screenData.url,
+        error = painterResource(id = drawable.ic_call_decline),
+        fallback = painterResource(id = R.drawable.ic_launcher_background),
+        contentScale = ContentScale.FillWidth,
+        placeholder = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Intro Image"
+    )
 }
