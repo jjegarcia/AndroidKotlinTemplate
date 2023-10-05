d6ff55c711c7bb9c84777146ee103541caca270e
jjegarcia <jjegarcia@hotmail.com>
2023-10-05 22:51:05 +0100

    displays characters in list view

--- app/src/main/java/com/example/androidkotlintemplate/MainViewModel.kt
+++ app/src/main/java/com/example/androidkotlintemplate/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.androidkotlintemplate
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,6 +10,10 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 enum class ApiStatus { LOADING, ERROR, DONE }
+data class CharacterInfo(
+    val url: String = "",
+    val description: String = ""
+)
 
 interface MainViewModel {
     val screenData: MutableStateFlow<ScreenData>
@@ -19,31 +22,12 @@ interface MainViewModel {
 @HiltViewModel
 class MainViewModelImpl @Inject constructor() : ViewModel(), MainViewModel {
 
-    // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<ApiStatus>()
 
-    // The external immutable LiveData for the request status
-    val status: LiveData<ApiStatus> = _status
+    private val _screenData = MutableStateFlow(ScreenData())
 
-    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
-    // with new values
-    private val _photos = MutableLiveData<String>()
-
-    // The external LiveData interface to the property is immutable, so only this class can modify
-    val photos: LiveData<String> = _photos
-
-
-    private val _screenData = MutableStateFlow(
-        ScreenData(
-            "Test",
-            ""
-        )
-    )
-
-    private fun getUrl(thumbnail: Thumbnail): String {
-        val string = "${thumbnail.path}/portrait_incredible.${thumbnail.extension}"
-        return string
-    }
+    private fun getUrl(thumbnail: Thumbnail): String =
+        "${thumbnail.path}/portrait_incredible.${thumbnail.extension}"
 
     override val screenData: MutableStateFlow<ScreenData>
         get() = _screenData
@@ -56,11 +40,10 @@ class MainViewModelImpl @Inject constructor() : ViewModel(), MainViewModel {
         viewModelScope.launch {
             _status.value = ApiStatus.LOADING
             try {
-                val characters: CharactersResponse = Api.retrofitService.getCharacters()
-                val thumbnail = getThumbnail(characters.data.results.get(0).name)
-                val url = getUrl(thumbnail)
-                screenData.value =
-                    screenData.value.copy(url = url)
+                val characters: List<CharacterInfo> =
+                    Api.retrofitService.getCharacters().data.results.map { getCharacterInfo(it.name) }
+                screenData.value = screenData.value.copy(characters = characters)
+                _status.value=ApiStatus.DONE
             } catch (e: Exception) {
                 Log.i("Error:", e.toString())
                 _status.value = ApiStatus.ERROR
@@ -68,15 +51,17 @@ class MainViewModelImpl @Inject constructor() : ViewModel(), MainViewModel {
         }
     }
 
-    private suspend fun getThumbnail(name: String): Thumbnail {
-        var thumbNail = Thumbnail("", "")
+    private suspend fun getCharacterInfo(name: String): CharacterInfo {
         try {
-            val response = Api.retrofitService.getCharacter(name)
-            thumbNail = response.data.results.get(0).thumbnail
+            val character = Api.retrofitService.getCharacter(name)
+            return CharacterInfo(
+                url = getUrl(character.data.results.get(0).thumbnail),
+                description = character.data.results.get(0).description
+            )
         } catch (e: Exception) {
             Log.i("Error:", e.toString())
             _status.value = ApiStatus.ERROR
         }
-        return thumbNail
+        return CharacterInfo()
     }
 }
--- app/src/main/java/com/example/androidkotlintemplate/Screen.kt
+++ app/src/main/java/com/example/androidkotlintemplate/Screen.kt
@@ -1,5 +1,8 @@
 package com.example.androidkotlintemplate
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,8 +13,8 @@ import androidx.core.R.drawable
 import coil.compose.AsyncImage
 
 data class ScreenData(
-    val screenName: String,
-    val url: String
+    val screenName: String = "Test",
+    val characters: List<CharacterInfo> = listOf()
 )
 
 @Composable
@@ -19,16 +22,23 @@ fun Greeting(viewModel: MainViewModelImpl, modifier: Modifier = Modifier) {
 
     val screenData = viewModel.screenData.collectAsState().value
 
-    Text(
-        text = "Hello ${screenData.screenName}!",
-        modifier = modifier
-    )
-    AsyncImage(
-        model = screenData.url,
-        error = painterResource(id = drawable.ic_call_decline),
-        fallback = painterResource(id = R.drawable.ic_launcher_background),
-        contentScale = ContentScale.FillWidth,
-        placeholder = painterResource(id = R.drawable.ic_launcher_background),
-        contentDescription = "Intro Image"
-    )
+    Column {
+        Text(
+            text = "Hello ${screenData.screenName}!",
+            modifier = modifier
+        )
+        LazyColumn {
+            itemsIndexed(screenData.characters) { index, item ->
+                Text(text = screenData.characters.get(index).description)
+                AsyncImage(
+                    model = screenData.characters.get(index).url,
+                    error = painterResource(id = drawable.ic_call_decline),
+                    fallback = painterResource(id = R.drawable.ic_launcher_background),
+                    contentScale = ContentScale.FillWidth,
+                    placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Intro Image"
+                )
+            }
+        }
+    }
 }
